002a1722f462989ab22ccf1318eda6dc01f00fd3
Abdullah Siddiqui <abzsiddiqui@outlook.com>
2026-07-17 11:08:09 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = ""
+        for char in s:
+            if char.isalnum():
+                cleaned += char
+        cleaned = cleaned.lower()
+        left = 0
+        right = len(cleaned) - 1
+
+        while right > left:
+            if cleaned[left] == cleaned[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
+        
