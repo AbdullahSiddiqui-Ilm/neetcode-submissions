8d93413468b5d137118c26d3196592edd93cd138
Abdullah Siddiqui <abzsiddiqui@outlook.com>
2026-09-16 14:44:00 +0100

    Add: merge-k-sorted-linked-lists - submission-5

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-5.py
@@ -0,0 +1,71 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        if not lists[0] and len(lists) == 1:
+            return None
+        
+
+        while len(lists) > 1:
+            i = 1
+            while i < len(lists):
+                lists[i - 1] = (self.mergeTwoLists(lists[i - 1], lists[i]))
+                lists.pop(i)  # this is not ideal as it shifts every element after once to the left, multiple times inside the loop
+                i += 2
+        return lists[0]
+
+        """ merged = []      #This version is O(n) space due to merged []
+        while len(lists) > 1:
+            i = 1
+            if len(lists) % 2 != 0:
+                merged.append(lists[-1])
+            while i < len(lists):
+                merged.append(self.mergeTwoLists(lists[i - 1], lists[i]))
+                i += 2
+            lists = merged
+            merged = []"""
+
+        return lists[0]
+
+    def mergeTwoLists(self, list1, list2) -> Optional[ListNode]:
+        if not list2:
+            return list1
+        
+        dummy = ListNode()
+        curr = dummy
+
+        p1 = list1
+        p2 = list2
+
+        while p1 and p2:
+            if p1.val > p2.val:
+                curr.next = p2
+                p2 = p2.next
+                curr = curr.next
+            else:
+                curr.next = p1
+                p1 = p1.next
+                curr = curr.next
+        if p1:
+            curr.next = p1
+            curr = curr.next
+        else:
+            curr.next = p2
+            curr = curr.next
+        return dummy.next
+
+
+"""
+    - define function mergeTwoLists
+    - do a merge sort on lists[i - 1] and lists[i]
+    - keep merging neighbouring lists until we have 1 list remaining
+    - using 'while len(lists) > 1'
+    - replace lists with merged lists every iteration (inside the while loop)
+    - once we have 1 list remaining, return head
+"""
